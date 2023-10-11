ff26512e1987ffe92081cc073dc38164e65ba2ae
Kolawole Daniel <kolawolecoke@gmail.com>
2023-10-11 21:51:39 +0100

    secrets

---
+++ secret.js
@@ -0,0 +1,4 @@
+export const API_KEY = "89678fff49c412f4abaf0586a993bbcc";
+export const API_URL = "https://api.themoviedb.org/3";
+export const IMAGE_PATH = "https://image.tmdb.org/t/p/w500";
+export const OVERLAY_PATH = "https://image.tmdb.org/t/p/original";
